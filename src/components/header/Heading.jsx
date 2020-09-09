7a3ca567caadeca146ecf56c10baec823e216d42
Luzie Bauch <luzie.bauch@tobit.com>
2020-09-09 17:47:13 +0200

    :lipstick: Design the lytfass

---
+++ src/components/header/Heading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../app.scss';
+
+const Heading = () => (
+    <div className="heading">
+        <p>
+            Es wurden schon
+            <strong> 75€ </strong>
+            gespendet!
+        </p>
+    </div>
+);
+export default Heading;
