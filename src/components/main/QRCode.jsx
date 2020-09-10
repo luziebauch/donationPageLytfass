6cfc4be94574ded681e1967a8b8ebe94873a7240
Luzie Bauch <luzie.bauch@tobit.com>
2020-09-10 15:34:59 +0200

    :bug: Fix QR Code

--- src/components/main/QRCode.jsx
+++ src/components/main/QRCode.jsx
@@ -3,7 +3,7 @@ import '../app.scss';
 
 const QRCode = () => (
     <div className="qrCodeContainer">
-        <div className="imageQR"><img src="../../images/Donation.png" alt="QR Code" width="300vw"/></div>
+        <div className="imageQR"><img src="https://tappqa.tobit.com/training/2020/LB/images/Donation.png" alt="QR Code" width="300vw"/></div>
         <div className="textQR">
             Gefällt Dir die Show?
             <br/>
