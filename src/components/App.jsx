1b4b85d6021f874678401bd194769958042c1c61
Luzie Bauch <luzie.bauch@tobit.com>
2020-09-11 12:11:24 +0200

    :construction: Fix the height of the content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,46 @@
 import { hot } from 'react-hot-loader/root';
-import React from 'react';
+import React, { PureComponent } from 'react';
 import './app.scss';
 // Components
 import MainBody from './main/MainBody';
 
-const App = () => (
+class App extends PureComponent {
+    constructor() {
+        super();
+        this.state = {
+            windowHeight: 900,
+        };
+    }
+
+    componentDidMount() {
+        const retVal = chayns.addWindowMetricsListener((e) => {
+            if (e !== this.state.windowHeight) {
+                this.setState({ windowHeight: e });
+            }
+            console.log(e);
+        }, true);
+        return () => chayns.removeWindowMetricsListener(retVal);
+    }
+
+    render() {
+        return (
+            <div className="screen" style={{ height: this.state.windowHeight }}>
+                <img src="https://cdn.pixabay.com/photo/2014/02/19/16/06/curtain-269920_1280.jpg" alt="" className="background"/>
+                <div className="content">
+                    <MainBody/>
+                </div>
+            </div>
+        );
+    }
+}
+/* const App = () => (
     <div className="screen">
         <img src="https://cdn.pixabay.com/photo/2014/02/19/16/06/curtain-269920_1280.jpg" alt="" className="background"/>
         <div className="content">
             <MainBody/>
         </div>
     </div>
-);
+); */
 
 export default App;
 export const HotApp = hot(App);
--- src/components/main/QRCode.jsx
+++ src/components/main/QRCode.jsx
@@ -4,11 +4,11 @@ import '../app.scss';
 const QRCode = () => (
     <div className="qrCodeContainer">
         <div className="imageQR">
-            <img
+            {/* <img
                 src="https://k8s.az-chayns.space/qrCodeGenerator/svg?value=https%3A%2F%2Fchayns.cc%2Fdnrvam&color=005EB8&text=Spenden&icon=TobitLogo"
                 alt="QR Code"
                 width="300vw"
-            />
+            /> */}
         </div>
         <div className="textQR">
             Gefällt Dir die Show?
