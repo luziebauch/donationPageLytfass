012fdecfd095c5f47c9e286840533d7577ad4795
Luzie Bauch <luzie.bauch@tobit.com>
2020-09-11 09:53:18 +0200

    :construction: QR Code URL

--- src/components/main/QRCode.jsx
+++ src/components/main/QRCode.jsx
@@ -3,7 +3,13 @@ import '../app.scss';
 
 const QRCode = () => (
     <div className="qrCodeContainer">
-        <div className="imageQR"><img src="https://tappqa.tobit.com/training/2020/LB/images/Donation.png" alt="QR Code" width="300vw"/></div>
+        <div className="imageQR">
+            <img
+                src="https://k8s.az-chayns.space/qrCodeGenerator/svg?value=https%3A%2F%2Fchayns.cc%2Fdnrvam&color=005EB8&text=Spenden&icon=TobitLogo"
+                alt="QR Code"
+                width="300vw"
+            />
+        </div>
         <div className="textQR">
             Gefällt Dir die Show?
             <br/>
