58ae74941addc6c87f7664e2c1a07b496fc7198a
Luzie Bauch <luzie.bauch@tobit.com>
2020-09-11 09:42:04 +0200

    :bug: Fix the margin of the list and the heading

--- src/components/main/ListDonors.jsx
+++ src/components/main/ListDonors.jsx
@@ -5,32 +5,34 @@ import { List, ListItem } from 'chayns-components/lib';
 const ListDonors = () => (
     <div className="list">
         <div className="thanks">Danke für Deine Spende!</div>
-        <List className="listPerson">
-            <ListItem
-                className="listItem"
-                title="Luzie Bauch"
-                image="https://sub60.tobit.com/u/2165503?size=200&nocache=true"
-                circle
-            />
-            <ListItem
-                className="listItem"
-                title="Noah Dahlhaus"
-                image="https://sub60.tobit.com/u/2210833?size=90"
-                circle
-            />
-            <ListItem
-                className="listItem"
-                title="Anonym"
-                image="https://sub60.tobit.com/Content/unknown_user.png"
-                circle
-            />
-            <ListItem
-                className="listItem"
-                title="Jana Walfort"
-                image="https://sub60.tobit.com/u/2392858?size=90"
-                circle
-            />
-        </List>
+        <div className="listPersonDiv">
+            <List className="listPerson">
+                <ListItem
+                    className="listItem"
+                    title="Luzie Bauch"
+                    image="https://sub60.tobit.com/u/2165503?size=200&nocache=true"
+                    circle
+                />
+                <ListItem
+                    className="listItem"
+                    title="Noah Dahlhaus"
+                    image="https://sub60.tobit.com/u/2210833?size=90"
+                    circle
+                />
+                <ListItem
+                    className="listItem"
+                    title="Anonym"
+                    image="https://sub60.tobit.com/Content/unknown_user.png"
+                    circle
+                />
+                <ListItem
+                    className="listItem"
+                    title="Jana Walfort"
+                    image="https://sub60.tobit.com/u/2392858?size=90"
+                    circle
+                />
+            </List>
+        </div>
     </div>
 );
 export default ListDonors;
